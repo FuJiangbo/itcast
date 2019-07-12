a7c37f33546c1d2a5d4c17b43c175df7a04e59db
FuJiangbo <jbo.fu@163.com>
2019-07-12 13:20:52 +0800

    添加工程到git

---
+++ ssm_demo/src/main/java/com/itheima/test/DemoTest.java
@@ -0,0 +1,12 @@
+package com.itheima.test;
+
+public class DemoTest {
+
+    public static void main(String[] args) {
+        System.out.println("----------------");
+        System.out.println("----------------");
+        System.out.println("----------------");
+        System.out.println("----------------");
+        System.out.println("----------------");
+    }
+}
